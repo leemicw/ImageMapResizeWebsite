91d7287b0115fd419ded6ea39482c07264e09ccc
Michael Lee <michael@michaelware.net>
2013-06-27 14:48:10 -0500

    Initial Commit of site with responsive design

---
+++ Source/Site/App_Start/BundleConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace ImageMapResizeWebsite
+{
+	public class BundleConfig
+	{
+		public static void RegisterBundles(BundleCollection bundles) {
+			bundles.Add(new StyleBundle("~/cssBundles/Shared/RootLayoutPage").Include("~/css/Shared/RootLayoutPage.css"));
+		}
+
+	}
+}
---
+++ Source/Site/ViewModels/Home/HomeIndexViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ImageMapResizeWebsite.ViewModels.Home
+{
+	public class HomeIndexViewModel
+	{
+	}
+}
