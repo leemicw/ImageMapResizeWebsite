ee3046bf4b5a7301fecd384bda499a5442b7e1dd
leemicw <michael@michaelware.net>
2013-06-28 14:34:49 -0500

    Added working sample to home page Added jQuery from Nuget Chagned Modernizer to pull from Nuget

--- Source/Site/App_Start/BundleConfig.cs
+++ Source/Site/App_Start/BundleConfig.cs
@@ -8,7 +8,13 @@ namespace ImageMapResizeWebsite
 		public static void RegisterBundles(BundleCollection bundles) {
 			bundles.Add(new StyleBundle("~/cssBundles/Shared/RootLayoutPage").Include("~/css/Shared/RootLayoutPage.css"));
 
-			bundles.Add(new ScriptBundle("~/jsBundles/root").Include("~/js/modernizr-2.6.2.js"));
+			bundles.Add(new ScriptBundle("~/jsBundles/root").Include(
+				"~/Scripts/modernizr-2.6.2.js",
+				"~/Scripts/jquery-1.10.1.js",
+				"~/js/jquery.ImageMapResize.js"
+				));
+
+			bundles.Add(new StyleBundle("~/cssBundles/Home/Index").Include("~/css/Home/Index.css"));
 		}
 
 	}
