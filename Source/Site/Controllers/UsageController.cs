65e2376385216951727b9efe9acece954f1a1bf3
delana k baker <duckpup@gmail.com>
2013-07-01 13:59:27 -0500

    Adding Usage Items
    adding usage controller, view Model, Index view and css/less files

---
+++ Source/Site/Controllers/UsageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ImageMapResizeWebsite.Controllers
+{
+    public class UsageController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        
+    }
+}
