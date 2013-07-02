1ad576426f04e0034bb6fcfacdafed94c00131b3
delana k baker <duckpup@gmail.com>
2013-07-02 15:34:52 -0500

    Usage View
    adding codeSample.jpg to view

--- Source/Site/App_Start/BundleConfig.cs
+++ Source/Site/App_Start/BundleConfig.cs
@@ -20,6 +20,7 @@ namespace ImageMapResizeWebsite
 
 			bundles.Add(new StyleBundle("~/cssBundles/Usage/Index").Include("~/css/Usage/Index.css"));
 
+			
 			//this needs to be seperate so it can be included after all the css files are loaded
 			bundles.Add(new ScriptBundle("~/jsBundles/respond").Include("~/Scripts/respond.js"));
 		}
